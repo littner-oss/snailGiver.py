9732614a05a67cfd1aa9ece075152f130b13f16d
littner-oss <69611050+littner-oss@users.noreply.github.com>
2021-07-02 14:34:11 -0400

    Add files via upload

---
+++ snailGiver.py
@@ -0,0 +1,11 @@
+snails = 0
+while snails < 5:
+    lolput = input("Enter 'Lol' to recieve a snail: ")
+    if lolput == ('Lol'):
+        snails += 1
+        print('You now have ' + str(snails) +' amount of snails!')
+    if lolput != ('Lol'):
+        print('Wrong!')
+print('You now have all of the required snails :)')
+print('______\n/  ___ \ \n|  / ,.\ |O    O\n| |  \d/ | \__/\n|__\_____/-(..)\n_/_____________/')
+input('Type anything to exit the program: ')
